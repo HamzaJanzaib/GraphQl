2c1ab4f1c961130ac3c8ef18bb8245ec14aafd7d
hamza-janzaib768 <hamzajanzaib90@gmail.com>
2026-03-18 21:15:49 +0500

    add test query to hello resolver and schema

--- server/src/graphql/resolvers/hello.ts
+++ server/src/graphql/resolvers/hello.ts
@@ -4,6 +4,7 @@ import { helloWord,newPost } from "@/controllers/graphql.js";
 export const helloResolver = {
     Query: {
         hello: helloWord,
+        test: () => "Test Query is working fine"
     },
     Mutation: {
         addPost:newPost,
